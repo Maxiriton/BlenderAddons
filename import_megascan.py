609926db67f86b535026493a360cfbaf6b00dc41
Henri Hebeisen <henri@rgba.fr>
2020-04-09 21:46:11 +0200

    Import script complete !

--- import_megascan.py
+++ import_megascan.py
@@ -1,4 +1,5 @@
 import bpy
+from mathutils import Vector
 from os import listdir,path
 from os.path import isfile, join, splitext,basename
 
@@ -11,7 +12,6 @@ def make_collection(collection_name, parent_collection):
         parent_collection.children.link(new_collection) # Add the new collection under a parent
         return new_collection
 
-folder_path = '/home/henri/Téléchargements/Brick_Rubble_rhlxZ_8K_3d_ms'
 def import_lod(path,material):
     for f in sorted(listdir(path)):
         name, extension = splitext(f)
@@ -30,32 +30,109 @@ def import_lod(path,material):
             bpy.context.scene.collection.objects.unlink(obj)
             obj.data.materials.append(material)
 
+def get_texture_from_folder(texture_folder, texture_name):
+    for f in listdir(texture_folder):
+        if texture_name in f:
+            return join(texture_folder,f)
+    return None
+
+
 
 def create_material(path):
-
-
     mat = bpy.data.materials.new(name=basename(path))
     mat.use_nodes = True
     mat.node_tree.nodes['Principled BSDF'].select = True
+    principled = mat.node_tree.nodes['Principled BSDF']
     mat.node_tree.nodes['Material Output'].select = False
     mat.node_tree.nodes.active = mat.node_tree.nodes['Principled BSDF']
 
+    coord = mat.node_tree.nodes.new('ShaderNodeTexCoord')
+    coord.location = Vector((-1000,0))
+
+
     texture_folder = join(path,'textures/')
-    files = []
-    for f in listdir(texture_folder):
-        files.append({"name":f,"name":f})
 
-    print(files)
-    print(texture_folder)
+    #albedo_map
+    albedo_path = get_texture_from_folder(texture_folder,'Albedo')
+    if albedo_path:
+        albedo = mat.node_tree.nodes.new('ShaderNodeTexImage')
+        albedo_image = bpy.data.images.load(filepath=albedo_path)
+        #albedo_image.colorspace_settings.name = 'Non-Color'
+        albedo.location = Vector((-500,350))
+        albedo.image = albedo_image
+        mat.node_tree.links.new(albedo.outputs['Color'], principled.inputs['Base Color'])
+        mat.node_tree.links.new(coord.outputs['UV'], albedo.inputs['Vector'])
 
-    bpy.data.objects['Cube'].data.materials.append(mat)
+    #roughness
+    roughness_path = get_texture_from_folder(texture_folder,'Roughness')
+    if roughness_path:
+        roughness = mat.node_tree.nodes.new('ShaderNodeTexImage')
+        roughness_image = bpy.data.images.load(filepath=roughness_path)
+        roughness_image.colorspace_settings.name = 'Non-Color'
+        roughness.location = Vector((-500,50))
+        roughness.image = roughness_image
+        mat.node_tree.links.new(roughness.outputs['Color'], principled.inputs['Roughness'])
+        mat.node_tree.links.new(coord.outputs['UV'], roughness.inputs['Vector'])
 
-    #save the current area
-    area = bpy.context.area.type
-    bpy.context.area.type = "NODE_EDITOR"
-    bpy.ops.node.nw_add_textures_for_principled(filepath=texture_folder, directory=texture_folder, files=files, relative_path=True)
-    bpy.context.area.type = area
+    #normal
+    normal_path = get_texture_from_folder(texture_folder,'Normal_')
+    if normal_path:
+        normal = mat.node_tree.nodes.new('ShaderNodeTexImage')
+        normal_image = bpy.data.images.load(filepath=normal_path)
+        normal_image.colorspace_settings.name = 'Non-Color'
+        normal.location = Vector((-500,-250))
+        normal.image = normal_image
+        normal_map_node = mat.node_tree.nodes.new('ShaderNodeNormalMap')
+        normal_map_node.location = Vector((-200,-250))
+        mat.node_tree.links.new(normal.outputs['Color'], normal_map_node.inputs['Color'])
+        mat.node_tree.links.new(normal_map_node.outputs['Normal'], principled.inputs['Normal'])
+        mat.node_tree.links.new(coord.outputs['UV'], normal.inputs['Vector'])
+
     return mat
 
-mat = create_material(folder_path)
-#import_lod(folder_path,bpy.data.materials[0])
+def clean_scene():
+    for obj in bpy.data.objects:
+        bpy.data.objects.remove(obj,do_unlink = True)
+
+    for collection in bpy.data.collections:
+        bpy.data.collections.remove(collection, do_unlink=True)
+
+def save_scene(folder_path):
+    file_name ='{}.blend'.format(basename(folder_path))
+    file_path = join(folder_path,file_name)
+    bpy.ops.wm.save_as_mainfile(filepath=file_path)
+
+def get_folder_path():
+    argv = sys.argv
+    if "--" not in argv:
+        argv = []  # as if no args are passed
+    else:
+        argv = argv[argv.index("--") + 1:]  # get all args after "--"
+
+    #the first argument must be the folder file_path
+    return argv[0]
+
+def main():
+    import sys       # to get command line args
+    import argparse  # to parse options for us and print a nice help message
+
+    # get the args passed to blender after "--", all of which are ignored by
+    # blender so scripts may receive their own arguments
+    argv = sys.argv
+
+    if "--" not in argv:
+        argv = []  # as if no args are passed
+    else:
+        argv = argv[argv.index("--") + 1:]  # get all args after "--"
+
+    folder_path = argv[0]
+    print(folder_path)
+    clean_scene()
+    mat = create_material(folder_path)
+    import_lod(folder_path,bpy.data.materials[0])
+    save_scene(folder_path)
+
+    print("batch job finished, exiting")
+
+if __name__ == "__main__":
+    main()
